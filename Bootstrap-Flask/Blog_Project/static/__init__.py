cf5dd266b65539b3a8eae695456ae5dcd171823a
Mermaid28 <121485529+Mermaid28@users.noreply.github.com>
2024-10-10 17:41:53 +0800

    Delete Bootstrap-Flask directory

--- Bootstrap-Flask/Blog_Project/static/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-# Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
-# @Project ：Bootstrap-Flask 
-# @File    ：__init__.py.py
-# @IDE     ：PyCharm 
-# @Author  ：A30041699
-# @Date    ：2024/10/9 12:07
